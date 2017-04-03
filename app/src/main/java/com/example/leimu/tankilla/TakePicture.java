7fbbaee63f9f0303a04cc122908a7d1b7eb01e93
Leimu <leimurautio@gmail.com>
2017-04-03 12:00:09 +0300

    +Cam

--- app/src/main/java/com/example/leimu/tankilla/TakePicture.java
+++ app/src/main/java/com/example/leimu/tankilla/TakePicture.java
@@ -1,19 +1,46 @@
 package com.example.leimu.tankilla;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by Leimu on 30.3.2017.
  */
 
+
+
 public class TakePicture extends Fragment {
+
+
+
+
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        Button camButton = (Button) getView().findViewById(R.id.btnCamera);
+
+        camButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
 
         getActivity().setTitle("Take Picture!");
     }
@@ -23,7 +50,30 @@ public class TakePicture extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+
+
         return inflater.inflate(R.layout.takepicture, container, false);
+    }
+
+    public void openCamera() {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+         startActivityForResult(intent, 0);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        ImageView capturedCam = (ImageView) getView().findViewById(R.id.capturedImage);
+
+        if(resultCode == RESULT_OK) {
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            capturedCam.setImageBitmap(bp);
+        }
     }
 
 }

5a8ab04a18b3fae78284c7a40ce1f1d48ac361c1
Leimu <leimurautio@gmail.com>
2017-04-04 15:16:03 +0300

    +quality of compression is bad = Not anymore -gallery-section, share-section, photo-names..etc

--- app/src/main/java/com/example/leimu/tankilla/TakePicture.java
+++ app/src/main/java/com/example/leimu/tankilla/TakePicture.java
@@ -4,8 +4,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
 import android.icu.util.Output;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -13,6 +15,7 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,6 +36,7 @@ import java.util.Random;
 
 import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
+import static android.graphics.Bitmap.CompressFormat.PNG;
 import static android.os.Environment.DIRECTORY_PICTURES;
 import static android.os.Environment.getExternalStorageDirectory;
 import static android.os.Environment.getExternalStoragePublicDirectory;
@@ -43,7 +47,7 @@ import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class TakePicture extends Fragment {
 
-
+    public static final int CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE = 1777;
 
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -74,8 +78,17 @@ public class TakePicture extends Fragment {
 
     public void openCamera() {
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
+       // Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      //   startActivityForResult(intent, 0);
+
+        Intent intent2 = new Intent("android.media.action.IMAGE_CAPTURE");
+        //File file = FileProvider.getUriForFile(file, getContext().getApplicationContext().getPackageName() + ".provider", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+        File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+
+       // FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", createImageFile());
+
+        intent2.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent2, CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE);
 
 
     }
@@ -87,18 +100,52 @@ public class TakePicture extends Fragment {
 
         ImageView capturedCam = (ImageView) getView().findViewById(R.id.capturedImage);
 
-
-        //Photo to imageview
-        if (resultCode == RESULT_OK) {
-            Bitmap bp = (Bitmap) data.getExtras().get("data");
-
-
-            capturedCam.setImageBitmap(bp);
-
-            System.out.println("Jou");
-            saveImage(bp);
-            System.out.println("Jou2");
+        //Check that request code matches ours:
+        if (requestCode == CAPTURE_IMAGE_FULLSIZE_ACTIVITY_REQUEST_CODE)
+        {
+            //Get our saved file into a bitmap object:
+            File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+            Bitmap bitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+            capturedCam.setImageBitmap(bitmap);
+            saveImage(bitmap);
         }
+    }
+
+    private Bitmap decodeSampledBitmapFromFile(String path, int reqWidth, int reqHeight) {
+
+        // BEST QUALITY MATCH
+
+        //First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        // Calculate inSampleSize, Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        int inSampleSize = 1;
+
+        if (height > reqHeight)
+        {
+            inSampleSize = Math.round((float)height / (float)reqHeight);
+        }
+        int expectedWidth = width / inSampleSize;
+
+        if (expectedWidth > reqWidth)
+        {
+            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
+            inSampleSize = Math.round((float)width / (float)reqWidth);
+        }
+
+        options.inSampleSize = inSampleSize;
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(path, options);
+
+
     }
 
     private void saveImage(Bitmap finalBitmap) {
@@ -115,7 +162,7 @@ public class TakePicture extends Fragment {
         if (file.exists ()) file.delete ();
         try {
             FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.PNG, 80, out);
+            finalBitmap.compress(PNG, 80, out);
             out.flush();
             out.close();
 
